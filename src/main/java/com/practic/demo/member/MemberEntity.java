cbd56304ccfae6cbe8c5e6c5f092e7fde9bfeddd
jak010 <bluetoon@naver.com>
2025-10-27 13:45:41 +0900

    refactor(member): MemberEntity Lombok 어노테이션 변경
    - 불필요한 import 제거 및 Lombok 어노테이션 순서 변경
    - 가독성 향상을 위한 코드 정리

--- src/main/java/com/practic/demo/member/MemberEntity.java
+++ src/main/java/com/practic/demo/member/MemberEntity.java
@@ -1,6 +1,5 @@
 package com.practic.demo.member;
 
-import com.practic.demo.member.exceptions.MemberStatusAlreadySet;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
